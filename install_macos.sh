63881d11546786b8a7df798b8e5930511006a159
CadeBrown <brown.cade@gmail.com>
2017-07-18 13:35:59 -0400

    updated installation

--- install_macos.sh
+++ install_macos.sh
@@ -6,15 +6,14 @@ if [ "$EUID" -ne 0 ];
   exit 1
 fi
 
-
-brew install SDL2 pkg-config mpich2 sdl2_ttf || exit 1
-
 cd /tmp/
 
-curl -L "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz" > lz4.tar.gz
-tar xfv lz4.tar.gz
-cd lz4-1.7.5
-make || exit 1
-make install || exit 1
+{ brew install pkg-config mpich sdl2 sdl2_ttf && \
+curl -L "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz" > lz4.tar.gz && \
+tar xfv lz4.tar.gz && \
+cd lz4-1.7.5 && \
+make && \
+make install; } || 
+{ echo "error while installing"; exit 1; }
 
 
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -12,12 +12,11 @@ fi
 CDIR="$PWD"
 cd /tmp/
 
-apt install libsdl2-dev libcr-dev mpich2 nfs-kernel-server nfs-common libsdl-ttf2.0-dev
-
-curl -L "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz" > lz4.tar.gz
-tar xfv lz4.tar.gz
-cd lz4-1.7.5
-make
-make install
+{ apt install libsdl2-dev mpich nfs-kernel-server nfs-common libsdl2-ttf-dev && \
+curl -L "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz" > lz4.tar.gz && \
+tar xfv lz4.tar.gz && \
+cd lz4-1.7.5 && \
+make && \
+make install; } || { echo "error installing"; exit 1; }
 
 
