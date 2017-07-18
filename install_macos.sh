aa4918e91d52276c3a1a058a96214bfba9d54f84
Cade Brown <brown.cade@gmail.com>
2017-07-18 13:38:25 -0400

    macos install

--- install_macos.sh
+++ install_macos.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-if [ "$EUID" -ne 0 ];
-  then echo "Please run as root"
-  exit 1
-fi
-
 cd /tmp/
 
 { brew install pkg-config mpich sdl2 sdl2_ttf && \
@@ -13,7 +7,7 @@ curl -L "https://github.com/lz4/lz4/archive/v1.7.5.tar.gz" > lz4.tar.gz && \
 tar xfv lz4.tar.gz && \
 cd lz4-1.7.5 && \
 make && \
-make install; } || 
+sudo make install; } || 
 { echo "error while installing"; exit 1; }
 
 
